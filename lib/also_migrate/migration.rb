956857661830f2d839ea3688db2665f4b0c5631c
John Weathers <johnweathers@gmail.com>
2011-12-06 17:06:44 -0500

    Fix issues with named indexes in PostgreSQL
     * PostgreSQL requires that index names be unique
       across tables
     * This code fix appends a suffix to make the
       duplicated indexes differ from the originals

--- lib/also_migrate/migration.rb
+++ lib/also_migrate/migration.rb
@@ -45,6 +45,13 @@ module AlsoMigrate
             elsif method == :add_index && !config[:indexes].nil?
               next
             else
+              if method == :add_index and args[2].present? and args[2][:name].present?
+                if args[2][:name].length >= 58
+                  args[2][:name] = "#{args[2][:name][0..-6]}_also".to_sym
+                else
+                  args[2][:name] = "#{args[2][:name]}_also".to_sym
+                end
+              end
               [ config[:destination] ].flatten.compact.each do |table|
                 if connection.table_exists?(table)
                   args[0] = table
